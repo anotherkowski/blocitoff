2430ebee879f544be3e5c64883e945cefae9dbc7
Ann Novakowski <annhnova@gmail.com>
2017-03-19 19:53:51 -0500

    Convert Devise views to slim-rails

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
-  describe "GET #home" do
+  describe "GET home" do
     it "returns http success" do
-      get :home
+      get :index
       expect(response).to have_http_status(:success)
+    end
+    it "renders the home template" do
+      get :home
+      expect(response).to render_template("home")
     end
   end
 
-  describe "GET #about" do
+  describe "GET about" do
     it "returns http success" do
       get :about
       expect(response).to have_http_status(:success)
