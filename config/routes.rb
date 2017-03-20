9a84b35c556092ab7844c31aa719b994037b0779
Ann Novakowski <annhnova@gmail.com>
2017-03-20 10:24:30 -0500

    Install mailcatcher for dev env. Config mailer for Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root 'pages#home'
 
   get 'pages/home'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319015146) do
+ActiveRecord::Schema.define(version: 20170320150643) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "item"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20170319015146) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
