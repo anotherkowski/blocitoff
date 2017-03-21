3dc9e5304b2a7c9c2f6935b1e704190faecd481a
Ann Novakowski <annhnova@gmail.com>
2017-03-20 21:32:58 -0500

    Create user#show view and update routes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    it "should have name and email attributes" do
+      expect(user).to have_attributes(name: user.name, email: user.email)
+    end
+  end
+
+  describe "view" do
+    before do
+      sign_in
+    end
+
+    it "returns HTTP success" do
+      get :show, {id: factory_user.id}
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #show view" do
+      get :show, {id: factory_user.id}
+      expect(response).to render_template :show
+    end
+  end
 end
