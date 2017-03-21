207f62fcc0aac7f3dc465ad9193ecac02b4107f2
Ann Novakowski <annhnova@gmail.com>
2017-03-20 21:36:12 -0500

    Add user name to users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320150643) do
+ActiveRecord::Schema.define(version: 20170321023523) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "item"
@@ -36,9 +36,11 @@ ActiveRecord::Schema.define(version: 20170320150643) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
