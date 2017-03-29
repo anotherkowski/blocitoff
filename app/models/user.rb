691be5dc6777797a0dac89aacb4eeac1dd8bc627
A. Novakowski <anotherkowski@users.noreply.github.com>
2017-03-29 18:28:06 -0400

    Update user model
    Remove comments and extra spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-
-  # allows a user to have many items associated with account
   has_many :items, dependent: :destroy
-
-  # validations
-
-
-  # Virtual attribute for authenticating by either username or email
-  # This is in addition to a real persisted field like 'username'
-  attr_accessor :login
+  attr_accessor :login # Virtual attribute for authenticating by either username or email
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
@@ -21,12 +11,10 @@ class User < ActiveRecord::Base
         where(conditions.to_h).first
       end
   end
-
+  
+  # Validations
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   # Only allow letter, number, underscore and punctuation.
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
-
-  validates :username, uniqueness: true, presence: true
-
-  validates :email, uniqueness: true, presence: true
-
 end
