5a7930f109ec960e8ac0f351c2dcda5283ba3ec4
A. Novakowski <anotherkowski@users.noreply.github.com>
2017-03-29 18:33:00 -0400

    Update users controller spec
    Remove trailing spaces and add spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
-
+describe 'GET #index' do
+    context 'when user is logged in' do
+      with :user
+      before do
+        sign_in user
+        get :index
+      end
+      it { is_expected.to respond_with :ok }
+      it { is_expected.to render_with_layout :application }
+      it { is_expected.to render_template :index }
+    end
+    context 'when user is logged out' do
+      before do
+        get :index
+      end
+      it { is_expected.to redirect_to new_session_path }
+      it { is_expected.to set_session(:return_to).to(user_path) }
+    end
+  end
 end
