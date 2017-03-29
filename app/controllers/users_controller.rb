e3b079f2f32394ba19ddbab7a2fce1226c947370
Ann Novakowski <annhnova@gmail.com>
2017-03-28 23:08:06 -0400

    Debug users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  after_action :verify_authorized
-  
+
   def show
     @user = User.find(params[:id])
     @items = @user.items
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: [] do
+  resources :users do
     resources :items, only: [:create]
   end
 
