8206cd19304123d136291a61ac1dc93411d6f642
A. Novakowski <anotherkowski@users.noreply.github.com>
2017-04-25 17:03:16 -0400

    Fixed weird spacing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   let(:my_user) { create(:user) }
 
-describe "GET #show" do
-  before do
-    sign_in(my_user)
-  end
+  describe "GET #show" do
+    before do
+      sign_in(my_user)
+    end
     it "displays user#show when logged in" do
       get :show, :id => my_user.id
       expect(response).to have_http_status(200)
